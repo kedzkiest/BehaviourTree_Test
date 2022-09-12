0eb80cfe6075cde8d2b429b5c4cf3f67e548b6b4
kedzkiest <kaede12kazu22@gmail.com>
2022-09-12 10:17:21 +0900

    added Node class

---
+++ BehaviourTree_Test/Assets/C#/Node.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public enum Status
+    {
+        SUCCESS,
+        RUNNING,
+        FAILURE
+    };
+
+    public Status status;
+    public List<Node> children = new List<Node>();
+    public int currentChild = 0;
+    public string name;
+
+    public Node() { }
+
+    public Node(string n)
+    {
+        name = n;
+    }
+
+    public virtual Status Process()
+    {
+        return children[currentChild].Process();
+    }
+
+    public void AddChild(Node n)
+    {
+        children.Add(n);
+    }
+}
