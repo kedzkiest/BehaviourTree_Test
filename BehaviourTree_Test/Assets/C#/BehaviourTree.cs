f9500494841ae5748991ef62103e9ad1dc6db5b8
kedzkiest <kaede12kazu22@gmail.com>
2022-09-12 10:23:51 +0900

    added BehaviourTree class

---
+++ BehaviourTree_Test/Assets/C#/BehaviourTree.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using UnityEngine;
+
+public class BehaviourTree : Node
+{
+    public BehaviourTree()
+    {
+        name = "Tree";
+    }
+
+    public BehaviourTree(string n)
+    {
+        name = n;
+    }
+
+    public override Status Process()
+    {
+        return children[currentChild].Process();
+    }
+
+    struct NodeLevel
+    {
+        public int level;
+        public Node node;
+    }
+
+    public void PrintTree()
+    {
+        string treePrintout = "";
+        Stack<NodeLevel> nodeStack = new Stack<NodeLevel>();
+        Node currentNode = this;
+        nodeStack.Push(new NodeLevel { level = 0, node = currentNode });
+
+        while(nodeStack.Count != 0)
+        {
+            NodeLevel nextNode = nodeStack.Pop();
+            treePrintout += new string('-', nextNode.level) + nextNode.node.name + "\n";
+            for(int i = nextNode.node.children.Count - 1; i >= 0; i--)
+            {
+                nodeStack.Push(new NodeLevel { level = nextNode.level + 1, node = nextNode.node.children[i] });
+            }
+        }
+
+        Debug.Log(treePrintout);
+    }
+}
