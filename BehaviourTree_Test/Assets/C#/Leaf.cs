1411f5590cf9592fed56179798d912a9e1e6b87d
kedzkiest <kaede12kazu22@gmail.com>
2022-09-12 10:27:01 +0900

    added Leaf class

---
+++ BehaviourTree_Test/Assets/C#/Leaf.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf : Node
+{
+    public delegate Status Tick();
+    public Tick ProcessMethod;
+
+    public Leaf() { }
+
+    public Leaf(string n, Tick pm)
+    {
+        name = n;
+        ProcessMethod = pm;
+    }
+
+    public override Status Process()
+    {
+        if(ProcessMethod != null)
+        {
+            return ProcessMethod();
+        }
+
+        return Status.FAILURE;
+    }
+}
