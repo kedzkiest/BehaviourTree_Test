ff6081a2d24b73f9db90f8fbcd1e09447b501d3d
kedzkiest <kaede12kazu22@gmail.com>
2022-09-13 12:37:01 +0900

    changed class name FisherBehaviour to FishermanBehaviour

--- BehaviourTree_Test/Assets/C#/FishermanBehaviour.cs
+++ BehaviourTree_Test/Assets/C#/FishermanBehaviour.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class FisherBehaviour : MonoBehaviour
+public class FishermanBehaviour : MonoBehaviour
 {
     #region Field
     public GameObject StartPoint;
